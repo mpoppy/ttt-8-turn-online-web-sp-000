70c9702f7bd1b729124557cb97dd06fa549a9834
Marc Poppy <poppy.marc@gmail.com>
2019-04-24 03:44:08 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,8 +10,9 @@ end
 
 def valid_move?(board, index)
   if index.between?(1,9)
-  if !position_taken?(board,index)
-    true
+    if !position_taken?(board,index)
+      true
+    end
   end
 end
 
