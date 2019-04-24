a39764ad1c67b4973ac81cf232824f2eb133f1a9
Marc Poppy <poppy.marc@gmail.com>
2019-04-24 03:49:35 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -17,7 +17,7 @@ def valid_move?(board, index)
 end
 
 def position_taken?(board, index)
-  board[ndex] = index
+  board[index] = index
   if (board[index] == " " || board[index] == "" || board[index] == nil)
     true
   end
