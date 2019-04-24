ab76d063cca5b484d4a545750ecf45a8b5f7364f
Marc Poppy <poppy.marc@gmail.com>
2019-04-24 03:39:47 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -21,8 +21,9 @@ end
 
 def position_taken?(board, index)
   taken = nil
-  if board[index] == " " || board[index] == "" || board[index] == nil
-    true
-  else false
+  if (board[index] == " " || board[index] == "" || board[index] == nil)
+    taken = false
+  else
+    taken = true
   end
 end
